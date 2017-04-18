39113d79333f40e148a9f6e9e10138787326f100
aisav <artur.isaverdyan@gmail.com>
2017-04-18 19:07:44 +0400

    print current number of elements of data queue in each second

--- src/com/art/Main.java
+++ src/com/art/Main.java
@@ -1,13 +1,15 @@
 package com.art;
 
 import java.util.LinkedList;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String args[]){
 
         BlockingQueue dataQueue = new LinkedBlockingQueue();
+
+        cheduledPrintQueueSize(dataQueue);
+
 
         for(int i=1; i<=10; i++) {
             Thread prodThread = new Thread(new Producer(dataQueue, i));
@@ -16,5 +18,15 @@ public class Main {
             prodThread.start();
             consThread.start();
         }
+    }
+
+    private static void cheduledPrintQueueSize(BlockingQueue dataQueue){
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(dataQueue.size());
+            }
+        }, 0, 1, TimeUnit.SECONDS);
     }
 }
